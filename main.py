ac5cad505d1419e0e52186e0b47516a70408205c
dariusholmes94 <dariusholmes2@gmail.com>
2024-05-07 20:47:08 -0700

    created my bookbot command

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world!")
+def main():
+    with open("books/frankenstein.txt") as f:
+    # opens book directory
+        file_contents = f.read()
+    # reads the book
+    print(file_contents)
+    # prints the text to the console
+
+main()
