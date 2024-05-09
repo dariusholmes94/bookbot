92d787aad8e6ce005e91f1259a841f19f700c0c4
dariusholmes94 <dariusholmes2@gmail.com>
2024-05-08 18:25:31 -0700

    Added a word count function after reading the book

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-def main():
-    with open("books/frankenstein.txt") as f:
-    # opens book directory
-        file_contents = f.read()
-    # reads the book
-    print(file_contents)
-    # prints the text to the console
+def word_count(book_contents):
+    # This functions takes a string containing the book contents and returns a word count.
+    words = book_contents.split()
+    word_count = len(words)
+    return word_count
 
+def main():
+    # This function reads the book contents and prints the book and word count.
+    book_contents = "books/frankenstein.txt"
+    with open(book_contents) as f:
+        file_contents = f.read()
+    
+    print(file_contents)
+    
+    count = word_count(file_contents)
+
+    print(f"Word count: {count}")
+
+    
 main()
